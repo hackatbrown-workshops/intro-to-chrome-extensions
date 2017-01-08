f7056425f4fc35f0be4695a85e194f32a6e44bda
ankitasharma1 <ankita_sharma1@brown.edu>
2017-01-07 23:27:53 -0500

    added code to make the twitter API call to tweet the link you are currently looking at
    reload and click on your chrome extension, you might be getting a
    'require' error, we'll fix this with browserify!

--- popup.js
+++ popup.js
@@ -10,4 +10,37 @@ chrome.tabs.query({'active': true}, function (tabs) {
     // console.log(), right click on your popup and click 'inspect'
     // then look at the console, you should see the current link
     // you are looking at printed
+
+    var Twit = require('twit');
+    // twit is Twitter's API client for node
+    // EXTRA: more on Twit:
+    // https://github.com/ttezel/twit/blob/master/README.md
+    // https://github.com/ttezel/twit
+
+    // do an 'npm install twit'
+
+    // generate keys 
+    // 1. go to https://apps.twitter.com/
+    // 2. log in with your twitter account
+    // 3. go to settings, mobile, add phone number -- you will be unable to use the
+    //    Twitter API if your account is not connected with your phone
+    // 4. go to keys and access tokens
+    // 5. copy and paste your consumer key/consumer_secret
+    // 6. click on generate access tokens and copy and paste them
+    //    here as well
+    var T = new Twit({
+      consumer_key:         'consumer_key',
+      consumer_secret:      'consumer_secret',
+      access_token:         'access_token',
+      access_token_secret:  'access_token_secret',
+      timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    })
+
+    //
+    //  tweet your link (update your status) which is stored in data
+    //
+    T.post('statuses/update', { status: data }, { mode: 'no-cors' }, function(err, data, response) {
+      console.log(data)
+    }) 
+
 });
